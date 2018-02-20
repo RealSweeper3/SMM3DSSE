17fc4507a9f6588c56c6a740e83c614d1c316126
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-02-20 15:15:48 -0600

    Update crc.c

--- source/crc.c
+++ source/crc.c
@@ -1,11 +1,11 @@
 #include <3ds.h>
 #include "crc.h"
 
-u32 addcrc(const void* inptr, u32 i, u32 diff) {
-    const u16* ptr = inptr;
-    i >>= 1;
-    u32 hash = 1;
-    while (i--)
-        hash += *(ptr++);
-    return diff + hash;
+u32 addcrc(const void *inptr, u32 i, u32 diff) {
+  const u16 *ptr = inptr;
+  i >>= 1;
+  u32 hash = 1;
+  while (i--)
+    hash += *(ptr++);
+  return diff + hash;
 }
