733d8e0de19c14fb4b2954917fdd786c14c0bc8e
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-07-25 17:19:53 -0500

    crc: fix formatting

--- source/crc.c
+++ source/crc.c
@@ -2,10 +2,10 @@
 #include "crc.h"
 
 u32 addcrc(const void *inptr, u32 i, u32 diff) {
-  const u16 *ptr = inptr;
-  i >>= 1;
-  u32 hash = 1;
-  while (i--)
-    hash += *(ptr++);
-  return diff + hash;
+    const u16* ptr = inptr;
+    i >>= 1;
+    u32 hash = 1;
+    while (i--)
+        hash += *(ptr++);
+    return diff + hash;
 }
