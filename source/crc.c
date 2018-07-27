002e503765941ca5300dcdc164f2e02c2d53358f
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-07-26 23:07:10 -0500

    fix formatting

--- source/crc.c
+++ source/crc.c
@@ -1,7 +1,7 @@
 #include <3ds.h>
 #include "crc.h"
 
-u32 addcrc(const void *inptr, u32 i, u32 diff) {
+u32 addcrc(const void* inptr, u32 i, u32 diff) {
     const u16* ptr = inptr;
     i >>= 1;
     u32 hash = 1;
