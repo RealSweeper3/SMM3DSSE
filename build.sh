21330c0f202e1be938ffbac5a7d6e01d49e40428
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-07-26 22:36:51 -0500

    download ctrlib using curl

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=/opt/devkitpro/devkitARM
+curl "https://raw.githubusercontent.com/valentinvanelslande/ctrlib/master/ctrlib.hpp" -o "include/ctrlib.hpp"
 make -j4
